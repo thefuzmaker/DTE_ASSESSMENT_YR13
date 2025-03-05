bb86ecacf28bd1e8af3e6a179d4240ea67272a8d
William <19191@rosmini.school.nz>
2025-03-06 10:00:45 +1300

    wrote the page outline

--- public/home/index.php
+++ public/home/index.php
@@ -7,9 +7,35 @@
 
 <!--import the initialize-->
 <?php require_once('../../private/initialize.php') ?>
-<?php include(SHARED_PATH . '/header.php') ?>
 <!--title of homepage-->
+<title>The Rosmini food drive hub</title>
 <!--link stylesheet and import header-->
 <link href="_stylesheet.css" rel="stylesheet">
+<?php include(SHARED_PATH . '/header.php') ?>
+<!--the main text of the homepage-->
+<div class="maintext">
+	<section>
+		<p class="hometxt">
+Rosmini College is a Catholic Boys’ School founded by the Rosminian order to provide a Catholic education to young men on Auckland’s North Shore. We aim to shape them into well-rounded young men ready to leave school who are able to make a positive contribution to society and are empowered to serve others by living out the founder’s maxims.
+		</p>
+
+		<ul>
+			<li><p class="hometxt">Our Mission: Rosmini College supports and develops well-rounded young men while keeping true to the maxims of Antonio Rosmini.  A holistic education in the Rosminian tradition that fosters a genuine encounter with Christ.</p></li>
+
+			<li><p class="hometxt">Our Vision: Christ-centred young men who have the values, attitudes, knowledge and skills to fulfil their life and be empowered to serve others.</p></li>
+
+			<li><p class="hometxt">Our Values: Dignity. Integrity. Fairness. Charity. Justice</p></li>
+
+			<li><p class="hometxt">Our Motto: Charity fulfils the law.</p></li>
+		</ul>
+	</section>
+	<section>
+		<p class="hometxt">
+Treaty of Waitangi: Rosmini College is committed to the principles of the Treaty of Waitangi and actively supports and encourages the achievement of its Maori pupils and fosters Maori culture.<br>
+
+Special Character: Rosmini College is a learning community that requires each student to pursue his fullest potential in spiritual, academic, social, cultural and co-curricular pursuits. The school will equip each student with the desire to keep the Law of Christ;  
+		</p>
+	</section>
+</div>
 <!--import the footer-->
 <?php include(SHARED_PATH . '/footer.php') ?> 
