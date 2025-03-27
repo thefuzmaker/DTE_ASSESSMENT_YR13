83637d83e86b1ce222e2d1aebf0ab37c3a6c5ee0
William <19191@rosmini.school.nz>
2025-03-28 12:54:15 +1300

    added a new feature where the pages will have a loading screen inbetween when you click on another page. still incomplete but we will fix in next sprint. uses css,php,javascript. sprint 2 end

--- private/shared/header.php
+++ private/shared/header.php
@@ -7,8 +7,10 @@
 
 <!DOCTYPE html>
 <html lang="en">
+			<!-- link the javascript -->
+<script href="../../public/javascript/script.js" defer></script>
 			<!-- link the stylesheet -->
-<link href='../../public/stylesheet/_styles.css' rel="stylesheet">
+<link href="../../public/stylesheet/_styles.css" rel="stylesheet">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,7 +20,7 @@
 <body>
 			<!-- creater the header for all pages -->
 <header>
-	<div class="navbar">
+	<nav class="navbar">
 		<div class="logo">
 			<!-- link in thr rosmini logo -->
 			<img src="https://projectspace.nz/rtdwnkzc/Max_William_Assessment/public/images/bf943936-2268-41af-a752-0743089ef9b5_wo.png" class="logo_img" alt="Rosmini Logo">
@@ -33,10 +35,21 @@
 			<li><a href="../../public/dummy/dummypage.php"  class="nav_button">How to contact</a></li>
 			<li><a href="../../public/dummy/dummypage.php"  class="nav_button">Start your volunteering Journey</a></li>
 		</ul>
-	</div>
+	</nav>
 </header>
+    		<!-- Loading Screen (Hidden by Default) -->
+    <div class="loading-screen">
+        <div class="spinner"></div>
+        <p>Loading...</p>
+    </div>
+
+    		<!-- Main Content Area -->
+    <div class="content">
+		 	<!-- Default page loaded on startup -->
+        <?php include '../../public/home/index.php'; ?>
+    </div>
 	<div class="headings">
-		<!-- heading for all the pages -->
+			<!-- heading for all the pages -->
 		<h1 class="headertext">The Rosminian Project</h1>	
 	</div>
 
